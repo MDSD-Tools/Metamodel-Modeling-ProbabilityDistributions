7a9ce49988b0a11610740d4c77ded7a6e45e78d5
Ralf Sieger <sieger@fzi.de>
2024-02-09 15:19:06 +0100

    added generic types to Conditionable

--- bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CPDEvaluator.java
+++ bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CPDEvaluator.java
@@ -5,8 +5,9 @@ import java.util.List;
 import tools.mdsd.probdist.api.entity.Conditionable.Conditional;
 
 public interface CPDEvaluator {
-	
-	public Double evaluate(CategoricalValue value, List<Conditional> conditionals);
-	
-	public ProbabilityDistributionFunction<CategoricalValue> getCPDGiven(List<Conditional> conditionals);
+
+    public Double evaluate(CategoricalValue value, List<Conditional<CategoricalValue>> conditionals);
+
+    public ProbabilityDistributionFunction<CategoricalValue> getCPDGiven(
+            List<Conditional<CategoricalValue>> conditionals);
 }
