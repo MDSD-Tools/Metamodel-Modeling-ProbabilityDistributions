e4e1d81b841a5b20996ad0861b7e100c28e1c2a5
Ralf Sieger <sieger@fzi.de>
2024-02-13 10:42:08 +0100

    added generic types

--- bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CPDRepresentation.java
+++ bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CPDRepresentation.java
@@ -5,16 +5,16 @@ import java.util.Optional;
 
 import tools.mdsd.probdist.api.entity.Conditionable.Conditional;
 
-public interface CPDRepresentation {
-	
-	public abstract static class CPDRepresentationBuilder {
-		
-		public abstract CPDRepresentation build();
-		
-	}
-	
-	public Optional<ProbabilityDistributionFunction<?>> getPDFGiven(List<Conditional> conditionals);
-	
-	public CPDRepresentationBuilder builder();
-	
+public interface CPDRepresentation<I extends Value<?>> {
+
+    public abstract static class CPDRepresentationBuilder<I extends Value<?>> {
+
+        public abstract CPDRepresentation<I> build();
+
+    }
+
+    public Optional<ProbabilityDistributionFunction<I>> getPDFGiven(List<Conditional<I>> conditionals);
+
+    public CPDRepresentationBuilder<I> builder();
+
 }
