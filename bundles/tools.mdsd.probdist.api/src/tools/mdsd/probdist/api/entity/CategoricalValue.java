75edfa932673a72b8fc54f6ccb9e89a9f9f3b88a
Ralf Sieger <sieger@fzi.de>
2024-11-12 08:37:14 +0100

    fixed CategoricalValue equals

--- bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CategoricalValue.java
+++ bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CategoricalValue.java
@@ -1,25 +1,43 @@
 package tools.mdsd.probdist.api.entity;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 import tools.mdsd.probdist.distributionfunction.Domain;
 
 public class CategoricalValue extends Value<String> {
-	
-	protected CategoricalValue(String value) {
-		super(value, Domain.CATEGORY);
-	}
-	
-	public static CategoricalValue create(String value) {
-		return new CategoricalValue(value);
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		return value.equals(other); 
-	}
-	
-	@Override
-	public String toString() {
-		return value; 
-	}
-	
+
+    protected CategoricalValue(String value) {
+        super(value, Domain.CATEGORY);
+    }
+
+    public static CategoricalValue create(String value) {
+        return new CategoricalValue(value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        if (other.getClass() != getClass()) {
+            return false;
+        }
+        CategoricalValue rhs = (CategoricalValue) other;
+        return new EqualsBuilder().append(value, rhs.value)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
 }
