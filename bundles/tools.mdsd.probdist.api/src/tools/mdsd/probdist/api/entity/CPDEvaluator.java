a22351e35135a02ab2f90a8d668e27ee6f36f677
Ralf Sieger <sieger@fzi.de>
2024-12-04 10:14:17 +0100

    introduced ISeedable interface

--- bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CPDEvaluator.java
+++ bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/entity/CPDEvaluator.java
@@ -3,10 +3,9 @@ package tools.mdsd.probdist.api.entity;
 import java.util.List;
 
 import tools.mdsd.probdist.api.entity.Conditionable.Conditional;
+import tools.mdsd.probdist.api.random.ISeedable;
 
-public interface CPDEvaluator {
-    void init(int seed);
-
+public interface CPDEvaluator extends ISeedable {
     public Double evaluate(CategoricalValue value, List<Conditional<CategoricalValue>> conditionals);
 
     public ProbabilityDistributionFunction<CategoricalValue> getCPDGiven(
---
+++ bundles/tools.mdsd.probdist.api/src/tools/mdsd/probdist/api/random/ISeedable.java
@@ -0,0 +1,5 @@
+package tools.mdsd.probdist.api.random;
+
+public interface ISeedable {
+    void init(int seed);
+}
